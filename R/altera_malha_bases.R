1c4f5ba9966569897e54b4d021055ed4269ed50f
João Pedro Dias <joaopedro.jpcd@gmail.com>
2024-08-22 09:42:19 -0300

    desenvolve_relacao_malha_2000_2010

--- R/altera_malha_bases.R
+++ R/altera_malha_bases.R
@@ -4,11 +4,13 @@ library(data.table)
 library(foreign)
 library(readxl)
 library(dplyr)
+library(ggmap)
+library(sf)
 
 source("R/gera_bases_grupos_ped_09_16.R")
 source("R/gera_bases_grupos_ped_16_19.R")
 
-teste <- read_sav("relacao/malhas2000e2010/Compatibiliza DF FINAL.sav")
+relacao_malhas <- read_sav("relacao/malhas2000e2010/Compatibiliza DF FINAL.sav")
 
 malha_2000 <- read_sf("Shapes/2000/5300108.SHP")
 malha_2010 <- read_sf("Shapes/2010/53SEE250GC_SIR.shp")
@@ -39,6 +41,7 @@ mapview(malha_2010,col.regions = "grey" ,col = "blue",alpha.regions = 0.2) + map
 
 
 teste_join_09_16 <- grupos_domic_ped_09_16 |> 
+  st_drop_geometry() |> 
   left_join(
     teste |> mutate(CD_GEOCODI = as.character(CODSETOR2010)),by = "CD_GEOCODI"
   )
@@ -47,6 +50,7 @@ mapview(teste_join_09_16 |> select(CODSETOR2000))
 mapview(teste_join_09_16 |> select(CODSETOR2010))
 
 teste_join_16_19 <- grupos_domic_ped_16_19 |> 
+  st_drop_geometry() |> 
   left_join(
     teste |> mutate(CD_GEOCODI = as.character(CODSETOR2010)),by = "CD_GEOCODI"
   )
@@ -54,3 +58,16 @@ teste_join_16_19 <- grupos_domic_ped_16_19 |>
 mapview(teste_join_16_19 |> filter(NM_SUBDIST %in% c("GAMA", "SANTA MARIA")) |> select(CODSETOR2000), col.regions = "grey" ,col = "blue",alpha.regions = 0.001) +
 mapview(teste_join_16_19 |> filter(NM_SUBDIST %in% c("GAMA", "SANTA MARIA")) |> select(CODSETOR2010), col.regions = "grey" ,col = "red",alpha.regions = 0.001 )
 
+
+malha_2000_join <- malha_2000 |> 
+  rename(CODSETOR2000 = ID_) |> 
+  mutate(CODSETOR2000 = as.numeric(CODSETOR2000)) |> 
+  full_join(relacao_malhas,by = "CODSETOR2000") 
+
+setores_ped_09_16 <- grupos_domic_ped_09_16 |> st_drop_geometry() |> mutate(CODSETOR2010 = as.double(CD_GEOCODI)) 
+setores_ped_16_19 <- grupos_domic_ped_16_19 |> st_drop_geometry() |> mutate(CODSETOR2010 = as.double(CD_GEOCODI))
+
+join_ped_09_16_malha_2000 <- malha_2000_join |> 
+  full_join(setores_ped_09_16) |> 
+  select(CODSETOR2000,ano,mes,domic,conglom,NM_SUBDIST) |> 
+  na.omit() |> unique() 
