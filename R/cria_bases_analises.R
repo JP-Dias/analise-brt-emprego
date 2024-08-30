d71dd9cebce43a83cdaf7f6e2b11eec04ce67070
João Pedro Dias <joaopedro.jpcd@gmail.com>
2024-08-30 20:54:06 -0300

    Update cria_bases_analises.R

--- R/cria_bases_analises.R
+++ R/cria_bases_analises.R
@@ -13,6 +13,12 @@ source("R/altera_malha_bases.R")
 ped <- readRDS("Rds/ped_vars.RDS")
 nova_ped <- readRDS("Rds/nova_ped_vars.RDS")
 
+# Estações BRT ----
+estacoes_brt <- readRDS("rds/geo_estacoes_brt.RDS")
+
+## Buffer Estações BRT ----
+buffer15 <- st_buffer(estacoes_brt, dist = 1500)
+buffer20 <- st_buffer(estacoes_brt, dist = 2000)
 
 ped |> left_join(join_ped_09_16_malha_2000)
 nova_ped |> left_join(join_ped_16_19_malha_2000)
