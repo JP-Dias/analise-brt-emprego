c3ce49a8d1a54fd6d01a35fa0d913a93660ce863
João Pedro Dias <joaopedro.jpcd@gmail.com>
2024-09-05 19:40:11 -0300

    Update 1 - estatisticas descritivas.R

--- analysis/R/1 - estatisticas descritivas.R
+++ analysis/R/1 - estatisticas descritivas.R
@@ -6,46 +6,179 @@ library(lubridate)
 library(zoo)
 
 # Leitura da base de dados
-base <- readRDS("analysis/dados/base.RDS")
+base <- readRDS("analysis/dados/base.RDS") #|> filter(NM_SUBDIST == "SANTA MARIA")
 
-# Criando o data frame com os dados do PIB
-pib <- data.frame(
-  ano = as.character(2003:2021),
-  pib_br = c(1.1, 5.8, 3.2, 4.0, 6.1, 5.1, -0.1, 7.5, 4.0, 1.9, 3.0, 0.5, -3.5, -3.3, 1.3, 1.8, 1.2, -3.3, 4.8),
-  pib_df = c(0.7, 5.0, 5.8, 5.5, 6.6, 4.5, 5.0, 4.4, 3.7, 0.8, 3.7, 2.0, -1.0, 0.0, 0.3, 1.7, 2.1,-2.6,3.0)
-)
 
-# Definindo as variáveis de intervalo
-buraco_inicio <- as.Date("2013-09-01")
-buraco_fim <- as.Date("2014-09-01")
+# Grupos ----
 
-# Função para preparação e plotagem dos dados
-plot_mm3 <- function(base, y_var, title) {
-  base |>
-    mutate(grupo_20 = as.factor(grupo_20),
-           ano_mes = paste0(base$ano,"-",ifelse(base$mes %in% 10:19,base$mes,paste0(0,base$mes))) |> ym()) |>
-    group_by(grupo_20, ano_mes) |>
-    summarise(media = mean({{ y_var }}, na.rm = TRUE)) |>
-    arrange(grupo_20, ano_mes) |>
-    mutate(mm_3 = zoo::rollmean(media, 12, fill = NA),
-           # Introduzindo NAs no intervalo desejado
-           mm_3 = ifelse(ano_mes >= buraco_inicio & ano_mes <= buraco_fim, NA, mm_3)) |>
-    right_join(data.frame(ano_mes = seq(from = as.Date("2009-01-01"), 
-                                        to = as.Date("2019-08-01"), 
-                                        by = "month"))) |>
-    ggplot(aes(x = ano_mes, y = mm_3, col = grupo_20)) +
-    scale_x_date(breaks = "2 years") +
-    geom_line(lwd = 1, na.rm = TRUE) + 
-    scale_color_manual(values = c("grey10","grey")) +
-    annotate("rect", xmin = as.Date("2013-08-01"), xmax = as.Date("2014-10-01"), ymin = -Inf, ymax = Inf, 
-             alpha = 0.2, fill = "grey50") + 
-    geom_vline(xintercept = as.Date("2014-06-14"), linetype = "dashed", color = "black", alpha = .9) +
-    theme_linedraw() + ggtitle(title)
-}
+base_g0 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(n = n()) 
 
-# Plotando os gráficos
-plot_mm3(base, ocupado, "Ocupado")
-plot_mm3(base |> filter(!is.na(ocupado)), informal, "Informal")
-plot_mm3(base |> filter(!is.na(ocupado)), rend_bruto, "Rendimento")
-plot_mm3(base |> filter(!is.na(ocupado)), idade, "Idade")
-plot_mm3(base |> filter(!is.na(ocupado)) , trab_plano, "Trabalha no Plano")
+g0 <- base_g0 |> 
+  ggplot(aes(x = ano, y = n,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Grupos")
+
+g0
+
+
+# Ocupados ----
+base_g1 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(ocupado, na.rm = TRUE)) 
+
+g1 <- base_g1 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Percentual de Ocupados")
+
+g1
+
+# Informais ----
+
+base_g2 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(informal, na.rm = TRUE)) 
+
+g2 <- base_g2 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Percentual de Informais")
+
+g2
+
+
+# Escolaridade ----
+# Ensino Superior
+base_g3 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(escol_sup_com, na.rm = TRUE)) 
+
+g3 <- base_g3 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Percentual de Superior Completo")
+
+g3
+
+# Ensino Médio
+
+base_g4 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(escol_med_com, na.rm = TRUE)) 
+
+g4 <- base_g3 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Percentual de Médio Completo")
+
+g4
+
+# Rendimento ----
+
+base_g5 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(rend_bruto, na.rm = TRUE)) 
+
+g5 <- base_g5 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Rendimento Bruto Médio")
+
+g5
+
+
+base_g6 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(rend_liquido, na.rm = TRUE)) 
+
+g6 <- base_g5 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Rendimento Líquido Médio")
+
+g6
+
+# Horas ----
+
+base_g7 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(horas_trab, na.rm = TRUE)) 
+
+g7 <- base_g7 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Horas Trabalhadas")
+
+g7
+
+
+# Trabaha no Plano ----
+
+base_g8 <- base |> 
+  filter(!is.na(ocupado)) |> 
+  mutate(grupo = ifelse(grupo_20 == 1,"Treated","Control")) |> 
+  group_by(ano,grupo) |>
+  summarise(media = mean(trab_plano, na.rm = TRUE)) 
+
+g8 <- base_g8 |> 
+  ggplot(aes(x = ano, y = media,col = grupo, group = grupo)) +
+  geom_line(lwd = 1) + 
+  scale_color_manual(values = c("grey10","grey")) +
+  # annotate("rect", xmin = "2013.8", xmax = "2014.8", ymin = -Inf, ymax = Inf, 
+  #          alpha = 0.2, fill = "grey50") + 
+  geom_vline(xintercept = "2014", linetype = "dashed", color = "black", alpha = .9) +
+  theme_linedraw() + ggtitle("Trabalha no Plano Piloto")
+
+g8
+
+
